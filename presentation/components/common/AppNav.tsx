44a339e2b7fc30a949bfa24bad5b5c427510ac04
MD. MUSTAK TAHSIN ABIR <153496690+inkand-paper@users.noreply.github.com>
2026-05-26 09:30:18 +0600

    fix: update AppNav.tsx

--- presentation/components/common/AppNav.tsx
+++ presentation/components/common/AppNav.tsx
@@ -153,7 +153,7 @@ export function AppNav() {
                     Log in
                   </Link>
                   <Link
-                    href="/auth/signup"
+                    href="/"
                     className="px-4 py-2 bg-purple-600 hover:bg-purple-700 text-white text-sm rounded-lg transition"
                   >
                     Sign up
